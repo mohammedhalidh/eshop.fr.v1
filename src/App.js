b42d94fb9506a68e6e6242e86f63c4338c2640ce
mohammedhalidh <124666625+mohammedhalidh@users.noreply.github.com>
2024-03-13 12:36:31 +0530

    Frontend updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 import Register from './User/Register';
+import Signin from './User/Signin';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 
 function App() {
   return (
     <div>
-      <Register/>
+      {/* <Signin/>
+      <Register/> */}
+      <BrowserRouter>
+      {<Routes>
+        <Route path="/Signup" element={<Register/>}></Route>
+        <Route path="/Signin" element={<Signin/>}></Route>
+
+      </Routes>}
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/User/Signin.js
@@ -0,0 +1,19 @@
+
+
+const Signin = () => {
+    return (
+        <div>
+            <h2 className="text-center">Sign In</h2>
+            <form>
+                <div className="text-center">
+                <input type="text" placeholder="Email or username"/><br/>
+                <input type="password" placeholder="Password"/>
+                </div>
+            </form>
+        </div>
+
+    );
+
+};
+
+export default Signin;
